5e8fb8355c3deddb08c468bfd37fb859afcecb1b
vhfmatks <vhfmatks@gmail.com>
2024-12-31 23:57:47 +0900

    ffff

--- app.py
+++ app.py
@@ -15,9 +15,12 @@ if uploaded_file is not None:
         faces = DeepFace.analyze(img_path=image_array,
                                 actions=['age'],
                                 detector_backend='retinaface')
-    for face in faces:
-        x, y, w, h = face['region']['x'], face['region']['y'], face['region']['w'], face['region']['h']
-        cv2.rectangle(image_array, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    st.image(image_array, caption='얼굴 감지 결과', use_container_width=True)
+    if len(faces) == 0:
+        st.write("얼굴을 감지할 수 없습니다.")
+    else:
+        for face in faces:
+            x, y, w, h = face['region']['x'], face['region']['y'], face['region']['w'], face['region']['h']
+            cv2.rectangle(image_array, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        st.image(image_array, caption='얼굴 감지 결과', use_container_width=True)
     st.write("감지된 얼굴 수:", len(faces))
     st.write("업로드한 사진의 크기는", uploaded_file.size, "바이트입니다.")
